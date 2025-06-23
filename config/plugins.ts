9c7039b4e6320d56bbfff77b6fd0c96f8b0517a9
mori <nabe7855@gmail.com>
2025-06-23 16:25:00 +0900

    fix: add second APP_KEYS value for Strapi session

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+// config/plugins.js
+
+module.exports = {
+  "documentation": {
+    enabled: true,
+    config: {
+      // オプション設定
+      info: {
+        name: "My API",
+        description: "API for my awesome project",
+        version: "1.0.0",
+      },
+    },
+  },
+};
